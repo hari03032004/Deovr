1d35cdb7736a899d231bf54507bec9af040bc0dc
Vasanthan <vasanthansrihari@gmail.com>
2024-06-26 07:42:59 +0530

    Icon except originals

--- src/components/SidebarData.jsx
+++ src/components/SidebarData.jsx
@@ -5,6 +5,13 @@ import * as IoIcons from "react-icons/io";
 import { FaUserAlt } from "react-icons/fa";
 import { FaVideo } from "react-icons/fa";
 import { IoMdVideocam } from "react-icons/io";
+import { CiCamera } from "react-icons/ci";
+import { MdOutlineSubscriptions } from "react-icons/md";
+import { CiHeart } from "react-icons/ci";
+import { MdOutlinePlaylistAddCheckCircle } from "react-icons/md";
+import { LiaCrownSolid } from "react-icons/lia";
+import { LiaDownloadSolid } from "react-icons/lia";
+
 import "./Sidebar.css";
 
 export const SidebarData = [
@@ -17,6 +24,7 @@ export const SidebarData = [
   {
     title: "PHOTOS",
     path: "/Photos",
+    icon: <CiCamera color="grey"/>,
     cName: "nav-text",
   },
   {
@@ -27,26 +35,31 @@ export const SidebarData = [
   {
     title: "GETVRPLAYER",
     path: "/GetVrPlayer",
+    icon: <LiaDownloadSolid color="grey"/>,
     cName: "nav-text",
   },
   {
     title: "PREMIUM",
     path: "/Premium",
+    icon: <LiaCrownSolid color="grey"/>,
     cName: "nav-text",
   },
   {
     title: "MY FAVOURITES",
     path: "/MyFavourites",
+    icon: <CiHeart color="grey"/>,
     cName: "nav-text",
   },
   {
     title: "MY SUBSCRIPTIONS",
     path: "/MySubscriptions",
+    icon: <MdOutlineSubscriptions color="grey"/>,
     cName: "nav-text",
   },
   {
     title: "PUBLIC PLAYLISTS",
     path: "/PublicPlayists",
+    icon: <MdOutlinePlaylistAddCheckCircle color="grey"/>,
     cName: "nav-text",
   },
 ];
