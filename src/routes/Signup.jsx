a4fb471185e23e021c6c5bbcaee87a4f54bddd57
Vasanthan <vasanthansrihari@gmail.com>
2024-07-05 13:05:23 +0530

    Signup page completed

---
+++ src/routes/Signup.jsx
@@ -0,0 +1,77 @@
+import Button from '../components/Button';
+import {useState} from 'react';
+import "../index.css";
+import { FaUserCircle } from "react-icons/fa";
+import { MdVpnKey } from "react-icons/md";
+import { FaEyeSlash } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+export default function Signup() {
+  const [eye,setEye]=useState(false);
+  function handleClick(){
+    setEye(!eye);
+    if(eye){
+      document.getElementById("password").type = "text";
+    }
+    else{
+      document.getElementById("password").type = "password";
+    }
+    
+  }
+  return (
+    <div className='sign-outer'>
+      <div className='sign-inner'>
+        <div className='sign-head'>
+          <h2>Sign Up</h2>
+        </div>
+        <div className='line-align'>
+          <div className="line"></div>
+        </div>
+        <div className='input-box'>
+          <MdEmail />
+          <input type='text' placeholder='Username' 
+          className='text' id='username'
+          />
+        </div>
+        <div className='input-box'>
+          <FaUserCircle />
+          <input type='email' placeholder='Email as login' 
+          className='text' id='username'
+          />
+        </div>
+        <div className='input-box'>
+          <MdVpnKey />
+          <input type='password' placeholder='Password -min. 6 symbols' 
+          className='text' id='password'
+          />
+          {eye? <FaEyeSlash onClick={handleClick}/> :  <FaEye onClick={handleClick}/>} 
+
+        </div>
+          <div className='checkbox-agree'>
+            <input type="checkbox" name="signed" id="sign" />
+            <label><span className='agree'>I agree to the </span><a className='TC'>Terms and Conditions</a></label>
+          </div>
+        <div >
+        </div>
+          <div className='checkbox-email'>
+            <input type="checkbox" name="signed" id="sign" />
+            <label className='email'>Email me what's new and personal deals</label>
+          </div>
+        <div >
+          <Button
+            name='CREATE ACCOUNT'
+            bgcolor="#39BAE5"
+            color="black"
+            width="300px"
+          />
+          <Button
+            name='SIGN IN'
+            bgcolor="transparent"
+            color="#39BAE5"
+            width="300px"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
